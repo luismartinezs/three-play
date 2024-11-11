b274d76af33627444ef4e34b4e6bb7b4aac1aad0
Luis Martinez <luismartinezwebdev@gmail.com>
2024-11-11 13:03:33 +0700

    loop setup

--- src/World/World.js
+++ src/World/World.js
@@ -5,19 +5,22 @@ import { createLights } from "./components/lights.js";
 
 import { createRenderer } from "./systems/renderer.js";
 import { Resizer } from "./systems/Resizer.js";
+import { Loop } from './systems/Loop.js';
 
 // "private" variables not exposed outside of the class
 // note: only works for singleton pattern
 let camera;
 let renderer;
 let scene;
+let loop;
 
 class World {
   // 1. Create an instance of the World app
   constructor(container) {
     camera = createCamera();
-    scene = createScene();
     renderer = createRenderer();
+    scene = createScene();
+    loop = new Loop(camera, scene, renderer);
     container.append(renderer.domElement);
 
     const cube = createCube();
@@ -26,13 +29,23 @@ class World {
     scene.add(cube, light);
 
     const resizer = new Resizer(container, camera, renderer);
-    resizer.onResize = () => {
-      this.render();
-    };
+
+    // we do not need this because we are using the loop
+    // resizer.onResize = () => {
+    //   this.render();
+    // };
   }
   // 2. Render the scene
   render() {
     renderer.render(scene, camera);
+  }
+
+  start() {
+    loop.start();
+  }
+
+  stop() {
+    loop.stop();
   }
 }
 
---
+++ src/World/systems/Loop.js
@@ -0,0 +1,22 @@
+import { Clock } from "three";
+
+class Loop {
+  constructor(camera, scene, renderer) {
+    this.camera = camera;
+    this.scene = scene;
+    this.renderer = renderer;
+  }
+
+  start() {
+    this.renderer.setAnimationLoop(() => {
+      // render a frame
+      this.renderer.render(this.scene, this.camera);
+    });
+  }
+
+  stop() {
+    this.renderer.setAnimationLoop(null);
+  }
+}
+
+export { Loop };
