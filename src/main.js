be62ba6842c1cfd9f413fc027bf85e8989a288f3
Luis Martinez <luismartinezwebdev@gmail.com>
2024-11-11 13:58:25 +0700

    add texture

--- src/main.js
+++ src/main.js
@@ -8,7 +8,10 @@ function main() {
   // 1. Create an instance of the World app
   const world = new World(container);
 
-  // 2. Render the scene
+  // produce a single frame (render on demand)
+  // world.render();
+
+  // start the loop (produce a stream of frames)
   world.start();
 }
 
