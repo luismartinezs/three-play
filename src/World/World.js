9b6e52f92a745d8cdfd91a7adbdbea107826442f
Luis Martinez <luismartinezwebdev@gmail.com>
2024-11-11 12:56:36 +0700

    resize

--- src/World/World.js
+++ src/World/World.js
@@ -1,7 +1,7 @@
 import { createCamera } from "./components/camera.js";
 import { createCube } from "./components/cube.js";
 import { createScene } from "./components/scene.js";
-import { createLights } from './components/lights.js';
+import { createLights } from "./components/lights.js";
 
 import { createRenderer } from "./systems/renderer.js";
 import { Resizer } from "./systems/Resizer.js";
@@ -26,6 +26,9 @@ class World {
     scene.add(cube, light);
 
     const resizer = new Resizer(container, camera, renderer);
+    resizer.onResize = () => {
+      this.render();
+    };
   }
   // 2. Render the scene
   render() {
